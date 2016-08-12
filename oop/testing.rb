5208afb70d077bf3d0af6b41898de78bc51728bf
kareningenuity <karenjxliu@hotmail.com>
2016-08-12 18:34:45 -0500

    updated rspec test to test all entry-related features

--- oop/testing.rb
+++ oop/testing.rb
@@ -24,25 +24,30 @@ RSpec.describe Entry do
       entry = Entry.new("happy", ["joyful"], ["sad"])
       entry.add_synonym("cheery")
       expect(entry.synonyms).to eq(["joyful", "cheery"])
-    end
-  end              
-  # end  
-  # describe '#synonyms' do
-  #   it 'should return joyful if call synonyms method' do
-  #     entry = Entry.new("happy", ["joyful"], ["sad"])
-  #     expect(entry.synonyms).to eq(["joyful"])
-  #   end              
-  # end  
-  # describe '#synonyms' do
-  #   it 'should return joyful if call synonyms method' do
-  #     entry = Entry.new("happy", ["joyful"], ["sad"])
-  #     expect(entry.synonyms).to eq(["joyful"])
-  #   end              
-  # end  
-  # describe '#synonyms' do
-  #   it 'should return joyful if call synonyms method' do
-  #     entry = Entry.new("happy", ["joyful"], ["sad"])
-  #     expect(entry.synonyms).to eq(["joyful"])
-  #   end              
-  # end          
+    end          
+  end  
+  describe '#add_antonym' do
+    it 'should return sad and gloomy if call antonyms method, after adding gloomy as an antonym' do
+      entry = Entry.new("happy", ["joyful"], ["sad"])
+      entry.add_antonym("gloomy")
+      expect(entry.antonyms).to eq(["sad", "gloomy"])
+    end          
+  end 
+  describe '#delete_synonym' do
+    it 'should return cheery if call synonyms method, after adding cheery as a synonym and deleting joyful' do
+      entry = Entry.new("happy", ["joyful"], ["sad"])
+      entry.add_synonym("cheery")
+      entry.delete_synonym("joyful")
+      expect(entry.synonyms).to eq(["cheery"])
+    end          
+  end  
+  describe '#delete_antonym' do
+    it 'should gloomy if call antonyms method, after adding gloomy as an antonym and deleting sad' do
+      entry = Entry.new("happy", ["joyful"], ["sad"])
+      entry.add_antonym("gloomy")
+      entry.delete_antonym("sad")
+      expect(entry.antonyms).to eq(["gloomy"])
+    end          
+  end            
 end
+
